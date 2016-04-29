ef3a31ab45ddf9a7541173f4f19bcb3450f6e3d5
bjrimell <barryrimell@gmail.com>
2016-04-29 12:06:53 +0100

    Added files via upload

--- js/todoList.js
+++ js/todoList.js
@@ -1,30 +1,38 @@
-		var app = angular.module("myTodoList", []);
-			app.controller("myCtrl", function($scope, $http) {
+var app = angular.module("myTodoList", []);
 
-			$http.get("/data.json").then(function(response) {
-        		$scope.myData = response.data.records;});
-
-			$scope.initialiseData = function()
-			{
-				$scope.products = [{todoText: "Tidy room", done:false}, {todoText: "Buy some milk", done:true}, {todoText: "Phone bank", done:false}]
-			}
-
-			$scope.initialiseData();
-			
-			$scope.addItem = function() {
-			$scope.errorText = "There is no error to report. Well done!";
-			if (!$scope.addMe) {return;}
-
-			if ($scope.products.indexOf($scope.addMe) == -1) {
-						$scope.products.push({todoText:$scope.addMe, done:false});
-				} else {
-					$scope.errorText = "This item is already in your todo list!"
-				}
-			}
-
-			$scope.updateList = function() {
-				$scope.tempList = $scope.products;
-				$scope.initialiseData();
-			}
-
+app.service("todoListService", function($http, $q)
+{
+	var deferred = $q.defer();
+	$http.get('data/data.json').then(function(response)
+	{
+		deferred.resolve(response);
 	});
+
+	this.getItems = function()
+	{
+		return deferred.promise;
+	}
+	
+})
+
+app.controller("myCtrl", function($scope, todoListService) {
+
+	var promise = todoListService.getItems();
+	promise.then(function(response)
+	{
+		$scope.products = response.data.TodoListData;
+	});
+
+$scope.addItem = function() {
+$scope.errorText = "There is no error to report. Well done!";
+if (!$scope.addMe) {return;}
+
+if ($scope.products.indexOf($scope.addMe) == -1) {
+			$scope.products.push({Title:$scope.addMe, Completed:false});
+			$scope.save();
+	} else {
+		$scope.errorText = "This item is already in your todo list!"
+	}
+}
+
+});
